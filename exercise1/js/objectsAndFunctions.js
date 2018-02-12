00232b3c2b3487e1d1c5b18f4733d5403d11b8f0
Stefaan Samyn <stefaan.samyn@hogent.be>
2018-02-13 00:02:35 +0100

    create the starter application

---
+++ exercise1/js/objectsAndFunctions.js
@@ -0,0 +1,23 @@
+const myBicycle = {
+	speed: 30,
+	gear: 1,
+	frameMaterial: 'carbon fibre'
+};
+
+// Voeg een methode accelerate toe aan myBicycle
+// De methode heeft 1 parameter, een percentage waarmee
+// de speed van myBicycle moet worden aangepast
+
+// Laat de fiets 25% versnellen. Log de speed voor en na
+// de methode aanroep.
+
+// Voeg een methode toe die de snelheid beoordeelt
+// De methode retourneert een string
+// speed [0..10[ 'Walking...'
+// speed [10..20[ 'Brakes on...'
+// speed [20..40[ 'Pushing...'
+// speed [40..60[ 'Peleton style...'
+// speed >= 60 'Sagan style...
+// Stel de speed van myBicycle in op 10
+// Test je methode door een in een lusje de speed in stapjes van 10% (accelerate)
+// van tot 100 te gaan en telkens de geretourneerde string te loggen.
